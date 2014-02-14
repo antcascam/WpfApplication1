d39679e377ca05e3672897f5dd565e2ceb889fa0
Antonio J Castaño <ajcastano86@gmail.com>
2014-02-14 18:27:19 +0100

    Primera subida

---
+++ WpfApplication1/DataSingleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class DataSingleton
+{
+    // Variable estática para la instancia, se necesita utilizar una función lambda ya que el constructor es privado
+    private static readonly Lazy<Singleton> instance = new Lazy<Singleton>(() => new Singleton());
+
+    //Variables
+    private int secondLeft;
+    private int secondRight;
+
+ 
+    // Constructor privado para evitar la instanciación directa
+    private DataSingleton()
+    {
+    }
+ 
+    // Propiedad para acceder a la instancia
+    public static Singleton Instance
+    {
+        get
+        {
+            return instance.Value;
+        }
+    }
+
+
+}
